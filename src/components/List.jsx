aaa0acb0005c89dc8b03fe1447fe17897f71774f
sankalp51 <sankalpkalangutkar51@gmail.com>
2024-01-27 21:38:03 +0530

    minor changes

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 const List = ({ list, validity }) => {
+    const [crossStyle, setCrossStyle] = React.useState(false);
+
+    function handleClick() {
+        setCrossStyle(prevState => !prevState);
+    }
+
     const content = <ul>
         {list.map((item, index) => {
-            return <li key={index}>{item}</li>;
+            return (
+                <li
+                    key={index}
+                    onClick={handleClick}
+                    style={{ textDecoration: crossStyle ? 'line-through' : null }}>
+                    {item}
+                </li>);
         })}
     </ul>
     return !validity ? <p style={{ color: 'red' }}>Cannot add empty item</p> : content
