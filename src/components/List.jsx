54f577dcc55d96f5ba6da0d80b2c0e111d7408ec
sankalp51 <sankalpkalangutkar51@gmail.com>
2024-01-27 21:23:10 +0530

    minor improvements

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 const List = ({ list, validity }) => {
     const content = <ul>
-        {list.map((item) => {
-            return <li>{item}</li>;
+        {list.map((item, index) => {
+            return <li key={index}>{item}</li>;
         })}
     </ul>
     return !validity ? <p style={{ color: 'red' }}>Cannot add empty item</p> : content
